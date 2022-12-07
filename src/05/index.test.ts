27696868ff872e61207e19d8e29bdb1caf53eb4c
Mario Ferrero <47794126+mariofdezzz@users.noreply.github.com>
2022-12-07 18:34:37 +0000

    feat<05>: add solve function test

--- src/05/index.test.ts
+++ src/05/index.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from 'asserts';
-import { findLastAlive } from './index.ts';
+import { findLastAlive, solve } from './index.ts';
 
 Deno.test('Challenge 05', async ({ step }) => {
 	await step('Example 01', () => {
@@ -7,5 +7,11 @@ Deno.test('Challenge 05', async ({ step }) => {
 		const { index } = findLastAlive(input);
 
 		assertEquals(index, 4);
+	});
+
+	await step('Solve', () => {
+		const input = new Array(10).fill(0).map((_, idx) => String(idx));
+
+		assertEquals(solve(input), '4-4');
 	});
 });
